5c3feca83880da48a6d8db15cf18a8a937d36046
LEESANGNAM <fgh0116@naver.com>
2024-01-04 23:04:38 +0900

    [Feat] Add Onboarding VC

--- SeSacSlack/OnBoardingViewController.swift
+++ SeSacSlack/OnBoardingViewController.swift
@@ -8,16 +8,54 @@
 import UIKit
 import SnapKit
 class OnBoardingViewController: UIViewController {
+    let startButton = CustomBackgroundTitleButton(title: "시작하기", color: Colors.brandGreen.color)
+    let titleLabel = {
+        let view = UILabel()
+        view.text = "새싹톡을 사용하면 어디서나 팀을 모을 수 있습니다."
+        view.font = Font.title1.font
+        view.numberOfLines = 2
+        view.textAlignment = .center
+        return view
+    }()
     
-    
-    
+    let iconImageView = {
+        let view = UIImageView()
+        view.image = Icon.onboarding.image
+        return view
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemBlue
+        view.backgroundColor = Colors.brandWhite.color
+        setHierarchy()
+        setConstraion()
         // Do any additional setup after loading the view.
     }
-
+    private func setHierarchy() {
+        view.addSubview(titleLabel)
+        view.addSubview(iconImageView)
+        view.addSubview(startButton)
+    }
+    private func setConstraion() {
+        titleLabel.snp.makeConstraints { make in
+            make.top.equalTo(view.safeAreaLayoutGuide).offset(39)
+            make.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(24)
+            make.width.equalTo(345)
+//            make.height.equalTo(60)
+        }
+        iconImageView.snp.makeConstraints { make in
+            make.top.equalTo(titleLabel.snp.bottom).offset(60)
+            make.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(13)
+            make.height.equalTo(368)
+            make.width.equalTo(iconImageView.snp.height)
+        }
+        startButton.snp.makeConstraints { make in
+            make.top.equalTo(iconImageView.snp.bottom).offset(153)
+            make.horizontalEdges.bottom.equalTo(view.safeAreaLayoutGuide).inset(24)
+            make.height.equalTo(44)
+        }
+        
+    }
 
 }
 
