0c4b6724e71c230848b2d9481a32cb3e74f1f76f
LEESANGNAM <fgh0116@naver.com>
2024-01-14 17:53:07 +0900

    [Feat] Add ViewModel

---
+++ SeSacSlack/Presentation/WorkSpace/Add/ViewModel/WorkSpaceAddViewModel.swift
@@ -0,0 +1,25 @@
+//
+//  WorkSpaceAddViewModel.swift
+//  SeSacSlack
+//
+//  Created by 이상남 on 1/14/24.
+//
+
+import Foundation
+
+class WorkSpaceAddViewModel {
+    
+    struct Input {
+        
+    }
+    
+    struct Output {
+        
+    }
+    
+    func transform(input: Input) -> Output {
+        
+        return Output()
+    }
+    
+}
