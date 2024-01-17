66f09ae9eb24bd2e4ea97854396ed3d91c5745f7
LEESANGNAM <fgh0116@naver.com>
2024-01-18 00:22:32 +0900

    [Fix] IconImageCollectionViewCell 변경 섹션마다 높이설정, 마지막 아이템 + 버튼으로 변경

---
+++ SeSacSlack/Common/Component/Cell/IconImageCollectionViewCell.swift
@@ -0,0 +1,102 @@
+//
+//  ChannelCollectionViewCell.swift
+//  SeSacSlack
+//
+//  Created by 이상남 on 1/17/24.
+//
+
+import UIKit
+
+class IconImageCollectionViewCell: BaseCollectionViewCell {
+    let iconImageView = UIImageView()
+    
+    let titleLabel = {
+        let view = UILabel()
+        view.text = "일반"
+        view.textColor = Colors.textSecondary.color
+        view.font = Font.body.fontWithLineHeight()
+        return view
+    }()
+    
+    let countButton: CustomBackgroundTitleButton = {
+        let button = CustomBackgroundTitleButton(title: "99", color: Colors.brandGreen.color)
+        // 폰트 크기 조절
+        button.titleLabel?.font = Font.caption.fontWithLineHeight()
+        return button
+    }()
+    
+    override func setHierarchy() {
+        addSubview(iconImageView)
+        addSubview(titleLabel)
+        addSubview(countButton)
+        
+    }
+    
+    override func setConstraint() {
+        iconImageView.snp.makeConstraints { make in
+            make.size.equalTo(18)
+            make.leading.equalToSuperview().offset(16)
+            make.centerY.equalToSuperview()
+        }
+        
+        titleLabel.snp.makeConstraints { make in
+            make.leading.equalTo(iconImageView.snp.trailing).offset(16)
+            make.centerY.equalToSuperview()
+        }
+        countButton.layer.cornerRadius = 8
+        countButton.snp.makeConstraints { make in
+            make.trailing.equalToSuperview().offset(-17)
+            make.leading.equalTo(titleLabel.snp.trailing).offset(16)
+            make.centerY.equalToSuperview()
+            make.width.equalTo(24)
+            make.height.equalTo(18)
+        }
+        
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        iconImageView.backgroundColor = nil
+        iconImageView.tintColor = nil
+        iconImageView.image = nil
+        titleLabel.text = nil
+        countButton.isHidden = true
+    }
+    
+    func setupchnnal(title: String){
+        iconImageView.image = Icon.hashtag.image
+        titleLabel.text = title
+    }
+    
+    func setupDM(image: UIImage?,title: String){
+        if let image {
+            iconImageView.image = image
+        } else {
+            iconImageView.image = UIImage(systemName: "person.fill")
+            iconImageView.backgroundColor = Colors.brandGreen.color
+            iconImageView.tintColor = Colors.brandWhite.color
+        }
+        titleLabel.text = title
+        iconImageView.layer.cornerRadius = 4
+        iconImageView.clipsToBounds = true
+        iconImageView.snp.updateConstraints { make in
+            make.size.equalTo(22)
+        }
+    }
+    
+    func setupLast(title: String) {
+        iconImageView.backgroundColor = nil
+        iconImageView.tintColor = nil
+        titleLabel.text = title
+        iconImageView.image = Icon.plus.image
+    }
+    func setupChatCount(_ count: Int?){
+        if let count {
+            countButton.isHidden = false
+            countButton.setTitle("\(count)", for: .normal)
+        } else {
+            countButton.isHidden = true
+        }
+    }
+    
+}
--- SeSacSlack/Presentation/WorkSpace/Home/Initail/WorkSpaceHomeInitViewController.swift
+++ SeSacSlack/Presentation/WorkSpace/Home/Initail/WorkSpaceHomeInitViewController.swift
@@ -13,7 +13,7 @@ import RxCocoa
 class WorkSpaceHomeInitViewController: BaseViewController {
     let testData = [
         ["일반","채널1","채널2","채널3","채널4","채널5","채널6","채널7","채널8","채널9","채널추가"],
-        ["사람1","사람2","사람3","사람4","사람5","사람6","사람7","사람8","사람9","사람10","사람11"],
+        ["사람1","사람2","사람3","사람4","사람5","사람6","사람7","사람8","사람9","사람10","새 메시지 시작"],
         ["팀원추가"]
     ]
     
@@ -54,6 +54,7 @@ class WorkSpaceHomeInitViewController: BaseViewController {
         configureHierarchy()
         configureDataSource()
         applyInitialSnapshots()
+        setUpBackgroundColors()
     }
     
     override func setHierarchy() {
@@ -66,7 +67,11 @@ class WorkSpaceHomeInitViewController: BaseViewController {
             make.height.equalTo(46)
         }
     }
-    
+    private func setUpBackgroundColors(){
+        view.backgroundColor = Colors.backgroundSecondar.color
+        workSpaceNaviBar.backgroundColor = Colors.backgroundSecondar.color
+        collectionView.backgroundColor = Colors.backgroundSecondar.color
+    }
     
 }
 
@@ -75,7 +80,6 @@ extension WorkSpaceHomeInitViewController {
     func configureHierarchy() {
         collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: createLayout())
         collectionView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
-        collectionView.backgroundColor = .systemGroupedBackground
         view.addSubview(collectionView)
         collectionView.snp.makeConstraints { make in
             make.top.equalTo(workSpaceNaviBar.snp.bottom)
@@ -92,11 +96,33 @@ extension WorkSpaceHomeInitViewController {
             
             guard let sectionKind = Section(rawValue: sectionIndex) else { return nil }
             
-            let section: NSCollectionLayoutSection
+            let item = NSCollectionLayoutItem(
+                layoutSize: NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
+                                                   heightDimension: .fractionalHeight(1.0)))
+            item.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0)
             
-            section = NSCollectionLayoutSection.list(using: .init(appearance: .sidebar), layoutEnvironment: layoutEnvironment)
-            section.contentInsets = NSDirectionalEdgeInsets(top: 00, leading: 0, bottom: 0, trailing: 0)
+            var containerGroup: NSCollectionLayoutGroup!
+            //            let section: NSCollectionLayoutSection
             
+            switch sectionKind {
+            case .channel:
+                containerGroup = NSCollectionLayoutGroup.vertical(
+                    layoutSize: NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
+                                                       heightDimension: .absolute(44)),
+                    subitem: item, count: 1)
+            case .directmessage:
+                containerGroup = NSCollectionLayoutGroup.vertical(
+                    layoutSize: NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
+                                                       heightDimension: .absolute(56)),
+                    subitem: item, count: 1)
+            case .addMember:
+                containerGroup = NSCollectionLayoutGroup.vertical(
+                    layoutSize: NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
+                                                       heightDimension: .absolute(44)),
+                    subitem: item, count: 1)
+                
+            }
+            let section = NSCollectionLayoutSection(group: containerGroup)
             return section
         }
         return UICollectionViewCompositionalLayout(sectionProvider: sectionProvider)
@@ -113,9 +139,28 @@ extension WorkSpaceHomeInitViewController {
         }
     }
     
-    func createOutlineCellRegistration() -> UICollectionView.CellRegistration<ChannelCollectionViewCell, String> {
-        return UICollectionView.CellRegistration<ChannelCollectionViewCell, String> { (cell, indexPath, text) in
-            cell.titleLabel.text = text
+    func createOutlineCellRegistration() -> UICollectionView.CellRegistration<IconImageCollectionViewCell, String> {
+        return UICollectionView.CellRegistration<IconImageCollectionViewCell, String> { (cell, indexPath, text) in
+            if let section = Section(rawValue: indexPath.section){
+                switch section {
+                case .channel:
+                    cell.setupchnnal(title: text)
+                    cell.setupChatCount(2)
+                    break
+                case .directmessage:
+                    cell.setupDM(image: nil, title: text)
+                    cell.setupChatCount(5)
+                    break
+                case .addMember:
+                    break
+                }
+                let itemCount = self.collectionView.numberOfItems(inSection: indexPath.section)
+                print("\(indexPath.section)섹션 아이템 갯수 ",itemCount)
+                if indexPath.item == itemCount - 1 {
+                    cell.setupLast(title: text)
+                    cell.setupChatCount(nil)
+                }
+            }
         }
     }
     
@@ -149,10 +194,9 @@ extension WorkSpaceHomeInitViewController {
         snapshot.appendSections(sections)
         dataSource.apply(snapshot, animatingDifferences: false)
         
-        //  outlines
-        var outlineSnapshot = NSDiffableDataSourceSectionSnapshot<Item>()
-        
         for category in sections {
+            //  outlines
+            var outlineSnapshot = NSDiffableDataSourceSectionSnapshot<Item>()
             
             var rootItem: Item
             if case Section.addMember = category {
