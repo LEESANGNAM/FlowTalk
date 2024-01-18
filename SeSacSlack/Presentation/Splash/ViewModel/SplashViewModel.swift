3f27f76a5ff824a0ac221e0b6a210362eff0c7b0
LEESANGNAM <fgh0116@naver.com>
2024-01-18 23:53:50 +0900

    [Feat] Rx+Extension

---
+++ SeSacSlack/Presentation/Splash/ViewModel/SplashViewModel.swift
@@ -0,0 +1,25 @@
+//
+//  SplashViewModel.swift
+//  SeSacSlack
+//
+//  Created by 이상남 on 1/18/24.
+//
+
+import Foundation
+
+class SplashViewModel {
+    
+    struct Input {
+        
+    }
+    
+    struct Output {
+        
+    }
+    
+    func transform(input: Input) -> Output {
+        
+        return Output()
+    }
+    
+}
