381e3a4950448aede728a605961935287c245c95
LEESANGNAM <fgh0116@naver.com>
2024-01-20 03:06:33 +0900

    [Fix] interceptor 변경 retry 구현, 직접구현 함수 삭제

--- SeSacSlack/Data/Network/Interceptor.swift
+++ SeSacSlack/Data/Network/Interceptor.swift
@@ -7,15 +7,61 @@
 
 import Foundation
 import Alamofire
-
+import RxSwift
 
 class Interceptor: RequestInterceptor {
+    let disposeBag = DisposeBag()
     func adapt(_ urlRequest: URLRequest, for session: Session, completion: @escaping (Result<URLRequest, Error>) -> Void) {
         var urlRequest = urlRequest
         let token = UserDefaultsManager.token
         urlRequest.setValue(token, forHTTPHeaderField: "Authorization")
         print("addtapter",urlRequest.headers)
         completion(.success(urlRequest))
+    }
+    
+    func retry(_ request: Request, for session: Session, dueTo error: Error, completion: @escaping (RetryResult) -> Void) {
+        print("리트라이 탐")
+        guard let _ = request.request?.value(forHTTPHeaderField: "Authorization"),
+              request.request?.value(forHTTPHeaderField: "RefreshToken") == nil,
+              let response = request.task?.response as? HTTPURLResponse,
+                    response.statusCode == 400 else {
+                  completion(.doNotRetryWithError(error))
+            print("리트라이 나감")
+                  return
+              }
+        
+        NetWorkManager.shared.request(type: RefreshTokenResponseDTO.self, api: .refresh)
+            .subscribe(with: self) { owner, value in
+                print("리프레시 성공")
+                UserDefaultsManager.token = value.accessToken
+                completion(.retry)
+            } onError: { owner, error in
+                if let tokenError = error as? RefreshErrorType {
+                    switch tokenError {
+                    case .validToken:
+                        completion(.doNotRetry)
+                    case .unownedUser:
+                        completion(.doNotRetry)
+                        ViewManager.shared.resetRootView()
+                    case .refreshTokenEnd:
+                        completion(.doNotRetry)
+                        ViewManager.shared.resetRootView()
+                    case .authFailed:
+                        completion(.doNotRetry)
+                        ViewManager.shared.resetRootView()
+                    }
+                } else {
+                    print("리프레시 토큰 에러:",error)
+                    completion(.doNotRetryWithError(error))
+                }
+            } onCompleted: { _ in
+                print("리프레시 완료")
+            } onDisposed: { _ in
+                print("리프레시 디스포즈")
+            }.disposed(by: disposeBag)
+
+        
+        
     }
         
 }
--- SeSacSlack/Data/Network/NetWorkManager.swift
+++ SeSacSlack/Data/Network/NetWorkManager.swift
@@ -61,29 +61,12 @@ final class NetWorkManager {
         case 400:
             print("실패 했는데 ------------")
             do {
-                print("여기 찍히나?")
+//                print("여기 찍히나?")
                 guard let data = response.data else { return }
-                print("데이터가 안찍히나",data)
+//                print("데이터가 안찍히나",data)
                 let error = try JSONDecoder().decode(ResponseErrorDTO.self, from: data)
                 print("에러코드 : ",error.errorCode)
                 let errorType = hendleError(code: error.errorCode)
-                if let commonError = errorType as? CommonErrorType {
-                    if commonError.rawValue == "E05" { // 토큰 만료
-                        print("토큰 에러 코드 : ",commonError.rawValue)
-                        changeToken(observer: observer)
-                    } else {
-                        observer.onError(commonError)
-                    }
-                } else if let refreshError = errorType as? RefreshErrorType {
-                    if refreshError.rawValue != "E04" {
-                        print("리프레쉬 에러")
-                        // 리프레쉬 토큰 에러 시 루트 뷰 변경
-                        ViewManager.shared.resetRootView()
-                        observer.onError(refreshError)
-                    }
-                } else if let loginError = errorType as? LoginSignUpErrorType {
-                    observer.onError(loginError)
-                }
                 observer.onError(errorType)
             } catch {
                 print("에러 변환 실패")
@@ -93,54 +76,6 @@ final class NetWorkManager {
             observer.onError(CommonErrorType.serverError)
         }
         
-    }
-    
-    private func changeToken<T: Decodable>(observer: AnyObserver<T>) {
-        
-        guard let originalRequest = originalRequest else {
-            return
-        }
-        print("토큰 재발급 들어옴")
-        request(type: RefreshTokenResponseDTO.self, api: .refresh)
-            .subscribe(with: self) { owner, token in
-                print("토큰 재발급", token.accessToken)
-                UserDefaultsManager.token = token.accessToken
-                // 기존 요청 재시도
-                owner.retryOriginalRequest(observer: observer, originalRequest: originalRequest)
-            } onError: { owner, error in
-                if let refreshError = error as? RefreshErrorType {
-                    if refreshError.rawValue != "E04" {
-                        print("리프레쉬 에러")
-                        // 리프레쉬 토큰 에러 시 루트 뷰 변경
-                        ViewManager.shared.resetRootView()
-                        observer.onError(refreshError)
-                    }
-                }
-                print("리프레시 토큰 실패")
-                observer.onError(error)
-            } onCompleted: { _ in
-                print("리프레쉬 완료")
-            } onDisposed: { _ in
-                print("디스포즈")
-            }
-    }
-    
-    // 기존 요청 재시도
-    private func retryOriginalRequest<T: Decodable>(observer: AnyObserver<T>, originalRequest: Router) {
-        print("기존 요청함")
-        request(type: T.self, api: originalRequest)
-            .subscribe(with: self) { owner, result in
-                // 기존 요청 성공 시 처리
-                observer.onNext(result)
-                observer.onCompleted()
-            } onError: { owner, error in
-                // 기존 요청 실패 시 처리
-                observer.onError(error)
-            } onCompleted: { _ in
-                print("기존 요청 재시도 완료")
-            } onDisposed: { _ in
-                print("기존 요청 재시도 디스포즈")
-            }
     }
     
     private func hendleError(code: String) -> Error {
