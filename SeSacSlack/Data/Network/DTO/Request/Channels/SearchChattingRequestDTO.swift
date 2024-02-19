704c68000b6e14a14bde020e7de49c2906cfc207
LEESANGNAM <fgh0116@naver.com>
2024-02-20 00:29:39 +0900

    [Feat] searchChatting DTO 추가

---
+++ SeSacSlack/Data/Network/DTO/Request/Channels/SearchChattingRequestDTO.swift
@@ -0,0 +1,8 @@
+//
+//  SearchChattingRequestDTO.swift
+//  SeSacSlack
+//
+//  Created by 이상남 on 2/19/24.
+//
+
+import Foundation
---
+++ SeSacSlack/Data/Network/DTO/Response/Channels/SearchChattingResponseDTO.swift
@@ -0,0 +1,8 @@
+//
+//  SearchChattingResponseDTO.swift
+//  SeSacSlack
+//
+//  Created by 이상남 on 2/19/24.
+//
+
+import Foundation
