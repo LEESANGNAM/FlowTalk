ddca7107b3b63aba1c8e10f52f2bd609179bed6f
LEESANGNAM <fgh0116@naver.com>
2024-02-21 01:44:32 +0900

    [Feat] 채팅 엔티티추가

--- SeSacSlack/Domain/Entity/ChannelChatting/ChannelChattingModel.swift
+++ SeSacSlack/Domain/Entity/ChannelChatting/ChannelChattingModel.swift
@@ -6,3 +6,14 @@
 //
 
 import Foundation
+
+struct ChannelChattingModel {
+    let chat_id: Int
+    let content: String?
+    let createdAt: String
+    let files: [String]
+    
+    let user_id: Int
+    let username: String
+    let userProfileImage: String?
+}
