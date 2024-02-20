6a4257feba61b8d91d726b20d85cfdd511f17026
LEESANGNAM <fgh0116@naver.com>
2024-02-21 00:45:59 +0900

    [Fix] 이미지 set함수 변경

--- SeSacSlack/Common/Extension/UIImageView+Extension.swift
+++ SeSacSlack/Common/Extension/UIImageView+Extension.swift
@@ -11,52 +11,40 @@ import Kingfisher
 extension UIImageView {
     func setImage(with urlString: String, frameSize imageSize: CGSize, placeHolder: String, completion: ((UIImage?) -> Void)? = nil) {
         self.kf.indicatorType = .activity
-        ImageCache.default.retrieveImage(forKey: urlString, options: nil) { result in
-            switch result {
-            case .success(let value):
-                if let image = value.image {
-                    // 캐시가 존재하는 경우
-                    print("캐시이미지 들어감")
-                    self.image = image
-                    completion?(image)
-                } else {
-                    // 캐시가 존재하지 않는 경우
-                    guard let url = URL(string: urlString) else {
-                        completion?(nil)
-                        return
-                    }
-                    
-                    let imageLoadRequest = AnyModifier { request in
-                        var requestBody = request
-                        requestBody.setValue(APIKey.key, forHTTPHeaderField: "SesacKey")
-                        requestBody.setValue(UserDefaultsManager.token, forHTTPHeaderField: "Authorization")
-                        return requestBody
-                    }
-                    let testSize = CGSize(width: imageSize.width * 3, height: imageSize.height * 3)
-                    let downSizeProcessor = DownsamplingImageProcessor(size: testSize) // 사이즈만큼 줄이기
-                    
-                    let resource = KF.ImageResource(downloadURL: url, cacheKey: urlString)
-                    self.kf.setImage(
-                        with: resource,
-                        options: [
-                            .processor(downSizeProcessor),
-                            .requestModifier(imageLoadRequest),
-                        ]) { result in
-                            switch result {
-                            case .success(let data):
-                                self.image = data.image
-                                completion?(data.image)
-                            case .failure(_):
-                                self.image = UIImage(named: placeHolder)
-                                completion?(nil)
-                            }
-                        }
-                }
-            case .failure(let error):
-                print(error.localizedDescription)
-                completion?(nil)
-            }
+        
+        let imageURLString = APIKey.baseURL + "/v1" + urlString
+        
+        guard let url = URL(string: imageURLString) else {
+            completion?(nil)
+            return
         }
+        
+        let imageLoadRequest = AnyModifier { request in
+            var requestBody = request
+            requestBody.setValue(APIKey.key, forHTTPHeaderField: "SesacKey")
+            requestBody.setValue(UserDefaultsManager.token, forHTTPHeaderField: "Authorization")
+            return requestBody
+        }
+        let testSize = CGSize(width: imageSize.width * 3, height: imageSize.height * 3)
+        let downSizeProcessor = DownsamplingImageProcessor(size: testSize) // 사이즈만큼 줄이기
+        
+        let resource = KF.ImageResource(downloadURL: url, cacheKey: imageURLString)
+        self.kf.setImage(
+            with: resource,
+            options: [
+                .processor(downSizeProcessor),
+                .requestModifier(imageLoadRequest),
+            ]) { result in
+                switch result {
+                case .success(let data):
+                    self.image = data.image
+                    completion?(data.image)
+                case .failure(_):
+                    self.image = UIImage(named: placeHolder)
+                    completion?(nil)
+                }
+            }
     }
+                
 }
 
