d7397e6d1577bf5c1e8f2caa71d5e0822d239f21
LEESANGNAM <fgh0116@naver.com>
2024-02-23 03:31:29 +0900

    [Feat] 저장용 엔티티 추가

--- SeSacSlack/Domain/Entity/ChannelChatting/ChannelChattingModel.swift
+++ SeSacSlack/Domain/Entity/ChannelChatting/ChannelChattingModel.swift
@@ -17,3 +17,21 @@ struct ChannelChattingModel {
     let username: String
     let userProfileImage: String?
 }
+
+
+struct SaveChannelChattingDTO {
+    let channel_id: Int
+    let channelName: String
+    let chat_id: Int
+    let content: String?
+    let createdAt: String
+    let files: [String]
+    let user: SaveChattingUser
+}
+
+struct SaveChattingUser: Decodable {
+    let user_id: Int
+    let email: String
+    let nickname: String
+    let profileImage: String?
+}
